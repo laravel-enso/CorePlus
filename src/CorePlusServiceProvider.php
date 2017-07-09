d3a7f2eaf46c4a758dbaa2e00ea148cd7b4b3d1b
mishu <mishu@xtelecom.ro>
2017-07-09 09:41:29 +0300

    updated service provider

--- src/CorePlusServiceProvider.php
+++ src/CorePlusServiceProvider.php
@@ -8,11 +8,11 @@ class CorePlusServiceProvider extends ServiceProvider
 {
     private $providers = [
         'Barryvdh\Debugbar\ServiceProvider',
-        'LaravelEnso\CnpValidator\CnpValidatorServiceProvider',
+        // 'LaravelEnso\CnpValidator\CnpValidatorServiceProvider',
         'LaravelEnso\CommentsManager\CommentsServiceProvider',
-        'LaravelEnso\ContactPersons\ContactPersonsServiceProvider',
+        // 'LaravelEnso\ContactPersons\ContactPersonsServiceProvider',
         'LaravelEnso\Core\CoreServiceProvider',
-        'LaravelEnso\DataImport\DataImportServiceProvider',
+        // 'LaravelEnso\DataImport\DataImportServiceProvider',
         'LaravelEnso\DocumentsManager\DocumentsServiceProvider',
         'LaravelEnso\Notifications\NotificationsServiceProvider',
     ];
