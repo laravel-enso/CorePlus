be25971ba7ea35d3bee51de3b82cff0bb8c436f7
Mihai <mihai.ocneanu@gmail.com>
2017-09-25 11:20:03 +0300

    removed manual loading of extra providers due to new provider autoloading

--- src/CorePlusServiceProvider.php
+++ src/CorePlusServiceProvider.php
@@ -6,15 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class CorePlusServiceProvider extends ServiceProvider
 {
-    private $providers = [
-        'Barryvdh\Debugbar\ServiceProvider',
-        'LaravelEnso\CommentsManager\CommentsServiceProvider',
-        'LaravelEnso\Contacts\ContactsServiceProvider',
-        'LaravelEnso\Core\CoreServiceProvider',
-        'LaravelEnso\DocumentsManager\DocumentsServiceProvider',
-        'LaravelEnso\Notifications\NotificationsServiceProvider',
-    ];
-
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
@@ -22,8 +13,6 @@ class CorePlusServiceProvider extends ServiceProvider
 
     public function register()
     {
-        foreach ($this->providers as $provider) {
-            $this->app->register($provider);
-        }
+        //
     }
 }
